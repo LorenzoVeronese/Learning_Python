c8f39de07431c05296b734d2c7703ae99907b511
Lory16 <55161983+Lory16@users.noreply.github.com>
2020-03-05 22:03:26 +0100

    Update 4-Comma_code.py

--- 4-Comma_code.py
+++ 4-Comma_code.py
@@ -1,6 +1,6 @@
 '''Write a function that takes a list value as an argument and returns a string with all the items separated by a comma and a space, 
-with and inserted before the last item. Your function should be able to work with any list value passed to it. Be sure to test the case where an empty list [] is 
-passed to your function.'''
+with and inserted before the last item. Your function should be able to work with any list value passed to it. Be sure to test the case 
+where an empty list [] is passed to your function.'''
 import copy
 
 el=[]
