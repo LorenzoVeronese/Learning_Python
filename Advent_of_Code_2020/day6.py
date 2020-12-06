ce0df6c609fec64cf6d699b6eca9bd93455c9fa5
Lorenzo Veronese <55161983+Lory16@users.noreply.github.com>
2020-12-06 13:36:20 +0100

    Add files via upload

---
+++ Advent_of_Code_2020/day6.py
@@ -0,0 +1,34 @@
+"""
+---PART ONE---
+"""
+"""
+PSEUDOCODE
+for line in file
+      if not void line and not last line
+            drop the final line's \n
+            add to list each new letter
+      else
+            sum += len(list)
+            list = []
+"""
+filename = "day6.txt"
+with open(filename, 'r') as ticks:
+      total_yes = 0
+      yes_list = []
+      for c_line in ticks:
+            if c_line != '\n':
+                  #the last line doesn't have \n at the end
+                  if c_line[len(c_line) - 1] == '\n':
+                        line = c_line[0 : len(c_line) - 1]
+                  else:
+                        line = c_line
+                  for i in line:
+                        if i not in yes_list:
+                              yes_list.append(i)
+            else:
+                  total_yes += len(yes_list)
+                  yes_list = []
+      #adding the last number
+      total_yes += len(yes_list)
+
+print(total_yes)
